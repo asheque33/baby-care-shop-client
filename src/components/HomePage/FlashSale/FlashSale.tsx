8a22361321f88508fa7c3ae0e44d50f557494bfb
asheque33 <ashequrrahman3333@gmail.com>
2024-09-19 03:39:56 +0600

    flash products section added in Home page

---
+++ src/components/HomePage/FlashSale/FlashSale.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { IProduct } from "@/types/product.type";
+import Link from "next/link";
+import ProductCard from "@/components/ProductCard";
+async function getProducts(): Promise<IProduct[]> {
+  const result = await fetch("http://localhost:4000/products", {
+    next: {
+      revalidate: 30,
+    },
+  });
+  const { data } = await result.json();
+  return data;
+}
+
+const FlashSale = async () => {
+  const data = await getProducts();
+  const products = data?.filter(
+    (product: IProduct) => product.isFlashSale === true
+  );
+  const flashSaleProducts: IProduct[] = products
+    .sort((a: IProduct, b: IProduct) => b._id.localeCompare(a._id))
+    .slice(0, 4);
+
+  return (
+    <section className=" px-4 md:px-8 py-8 lg:px-12">
+      <div className=" flex justify-between items-center mb-2 md:mb-4">
+        <span className="text-lg md:text-2xl lg:text-3xl font-bold ">
+          Flash Sale
+        </span>
+        <Button asChild>
+          <Link href="/flash-sale">View All</Link>
+        </Button>
+      </div>
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-4  md:gap-6 py-6">
+        {flashSaleProducts?.map((product: IProduct) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default FlashSale;
