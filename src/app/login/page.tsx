e98ad16f4894dbe79e4a73f7d8b5f1c9260784b8
asheque33 <ashequrrahman3333@gmail.com>
2024-09-25 23:21:09 +0600

    user logged in successfully

---
+++ src/app/login/page.tsx
@@ -0,0 +1,38 @@
+import LoginComponent from "@/components/Login/LoginComponent";
+import Container from "@/components/Shared/Container/Container";
+import Link from "next/link";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <Container className="h-screen flex items-center justify-center">
+      <div
+        id="login-content"
+        className="bg-[#F5EFE6]  flex justify-center items-center  w-4/5 md:w-3/5 lg:w-2/5  p-3.5 rounded-xl"
+      >
+        <div className="flex flex-col items-center w-full">
+          <section className="text-center mb-4">
+            <h1 className="mb-1.5 text-xl md:text-3xl lg:text-4xl font-semibold text-blue-500/80">
+              Login!!
+            </h1>
+            <p className="ms-4 italic text-slate-600/75 text-sm">
+              Create an account to enjoy all the benefits.
+            </p>
+          </section>
+          <LoginComponent />
+          <p className="my-1.5">
+            Don&apos;t have an account?{" "}
+            <Link
+              className="text-blue-500/80 font-semibold text-lg"
+              href={`/register`}
+            >
+              Create an account
+            </Link>
+          </p>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default LoginPage;
