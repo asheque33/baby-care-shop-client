2aab149d353da8044c68f2bb3a6578a9ce444717
asheque33 <ashequrrahman3333@gmail.com>
2024-10-03 23:45:17 +0600

    include user dashboard

---
+++ src/app/(withDashboardLayout)/dashboard/my-orders/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { UserOrdersComponents } from "../../DashComponents/User/MyOrders/OrdersComponent";
+
+const UserMyOrdersPage = () => {
+  return (
+    <div>
+      <UserOrdersComponents />
+    </div>
+  );
+};
+
+export default UserMyOrdersPage;
